2118645b209a8c158d57a54580d509bbaaf3ccdc
Javier Armendáriz <ev14photo@gmail.com>
2024-09-06 12:14:42 +0200

    Add files via upload

---
+++ js/snowstorm_client.js
@@ -0,0 +1,51 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+		function log( message ) {
+			$( "<div>" ).text( message ).prependTo( "#log" );
+			$( "#log" ).scrollTop( 0 );
+		}
+
+const codedfields = document.getElementsByClassName("coded");
+let codedfieldsLeft = codedfields.length;
+for (const codedfield of codedfields) {
+console.log(JSON.stringify(codedfield.id))
+		$( "#"+codedfield.id).autocomplete({
+
+			source: function( request, response ) {
+				configs = jQuery.parseJSON($("#"+codedfield.id).attr("config"));
+				$("#"+codedfield.id).addClass("ui-autocomplete-loading" );
+				$.ajax( {
+					type: 'POST',
+					url: "search.php?text="+$("#"+codedfield.id).val(),
+					dataType: "json",
+					data: {
+						term: request.term,
+						server: configs.server,
+						branch: configs.branch,
+						operation: configs.operation,
+						offset :configs.offset,
+						limit: configs.limit,
+						termactive:configs.termactive,
+						groupbyconcept: configs.groupbyconcept,
+						semantictag: configs.semantictag,
+						method: configs.operation,
+						descriptions_sub_path: configs.descriptions_sub_path,
+						codes_sub_path: configs.codes_sub_path,
+						save_logs:configs.save_logs
+					},
+					success: function( data ) {
+						$("#"+codedfield.id).removeClass("ui-autocomplete-loading" );
+						response( data );
+					}
+				} );
+			},
+			minLength: 2,
+			select: function( event, ui ) {
+				configs2 = jQuery.parseJSON($("#"+codedfield.id).attr("config"));
+				$("#"+configs2.code_field_id).val(ui.item.id);
+				log( "Seleccionado para el campo : " +codedfield.id + " la descripci\xF3n " + ui.item.value + " con c\xF3digo Snomed CT: " + ui.item.id );
+			}
+		} );
+}
+
+});
